7da3462dfae1eb87ab4f048feeb24e49c8bf1b80
KaizerPaul <anurag.paul03@gmail.com>
2024-07-24 14:01:57 +0530

    First N natural Numbers.py

--- First N natural Numbers.py
+++ First N natural Numbers.py
@@ -1,6 +1,13 @@
+/ While Loop
 num=int(input("Enter a number: "))
 
 i=1
 while i<=num:
     print(i)
     i=i+1  
+
+/ For Loop
+num = int(input("Insert a number:"))
+
+for i in range(1, num+1):
+    print(i, end=" ")
